7cfbb2c6b9c04614541eeb37862059cecc355a1e
Your Nameemail <shareef.99997@gmail.com>
2023-12-20 01:44:24 +0300

    text color

--- lib/features/authentication/screens/widgets/onboarding_skip.dart
+++ lib/features/authentication/screens/widgets/onboarding_skip.dart
@@ -1,8 +1,10 @@
 import 'package:drip/features/authentication/controllers/onboarding_controller.dart';
 import 'package:drip/utils/constants/text_strings.dart';
+import 'package:drip/utils/theme/widget_themes/text_theme.dart';
 import 'package:flutter/material.dart';
 import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/device/device_utility.dart';
+import '../../../../utils/helpers/helper_functions.dart';
 
 
 class OnBoardingSkip extends StatelessWidget {
@@ -12,7 +14,8 @@ class OnBoardingSkip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(top: TDeviceUtils.getAppBarHeight(),right: TSizes.defaultSpace, child: TextButton(onPressed: () =>OnBoardingController.Instance.skipPage(), child: Text(TTexts.skip),)
+    final dark = THelperFunctions.isDarkMode(context);
+    return Positioned(top: TDeviceUtils.getAppBarHeight(),right: TSizes.defaultSpace, child: TextButton(onPressed: () =>OnBoardingController.Instance.skipPage(), child: Text(TTexts.skip,style:dark? TTextTheme.darkTextTheme.bodyLarge :TTextTheme.lightTextTheme.bodyLarge ,),)
       
       
     );
