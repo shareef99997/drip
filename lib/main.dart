cf0d4afc1cc9be12e9a56ade1be3ee7c1933051f
Your Nameemail <shareef.99997@gmail.com>
2023-12-22 14:12:22 +0300

    initialise firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,36 @@ import 'package:drip/features/shop/controllers/cart_controller.dart';
 import 'package:drip/features/shop/controllers/categories_controller.dart';
 import 'package:drip/features/shop/controllers/checkout_controller.dart';
 import 'package:drip/features/shop/controllers/product_controller.dart';
+import 'package:drip/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:drip/app.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
-void main() {
+Future<void> main() async {
+
+  Get_init();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+  // .then(
+  //   (FirebaseApp value) => Get.put(AuthenticationRepository))
+  ;
+  
+
+  //Todo: Add Widgets Binding
+  //Todo: Init Local Storage
+  //Todo: Await Native Splash
+  
+  //Todo: Initialize Authentication 
+  
+  runApp(const App());
+}
+
+
+
+
+
+void Get_init() {
   WidgetsFlutterBinding.ensureInitialized();
   Get.put(CartController());
   Get.put(CategoryController());
@@ -17,12 +41,4 @@ void main() {
   Get.put(BrandController());
   Get.put(AddressController());
   Get.put(CheckoutController());
-  
-  //Todo: Add Widgets Binding
-  //Todo: Init Local Storage
-  //Todo: Await Native Splash
-  //Todo: Initialize Firebase
-  //Todo: Initialize Authentication 
-  
-  runApp(const App());
 }
